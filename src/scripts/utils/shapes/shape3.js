7e872945fb799a05ecafe49990b7ecb9c574f89e
victorcngo <hello@victorcango.fr>
2022-12-01 14:49:55 +0100

    refactor(utils/shapes): put points in dedicated files

---
+++ src/scripts/utils/shapes/shape3.js
@@ -0,0 +1,14 @@
+export const shape3Points = [
+  {
+    x: 0,
+    y: 0,
+  },
+  {
+    x: 100,
+    y: -25,
+  },
+  {
+    x: 75,
+    y: 75,
+  },
+]
---
+++ src/scripts/utils/shapes/shape5.js
@@ -0,0 +1,14 @@
+export const shape5Points = [
+  {
+    x: 0,
+    y: 0,
+  },
+  {
+    x: 50,
+    y: -50,
+  },
+  {
+    x: 100,
+    y: 0,
+  },
+]
